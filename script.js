c23eae1f12f680144e6836dfd997e661e61c3ddb
alladasaivamsi <109912485+alladasaivamsi@users.noreply.github.com>
2023-03-29 15:44:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 function threeSum(arr, target) {
 // write your code here
-  
+  let min = Number.MAX_VALUE; 
+  let ans = 0;
+  for(let i=0; i<arr.length; i++)
+	  {
+		  for(j=i+1; j<arr.length; j++)
+			  {
+				  for(k=j+1; k<arr.length; k++)
+					  {
+						  let sum = arr[i] + arr[j] + arr[k];
+						  if(Math.abs(sum - target) <= min)
+						  {
+							  min = Math.abs(sum - target);
+							  ans = sum;
+						  }
+					  }
+			  }
+	  }
+	return ans;
 }
 
 module.exports = threeSum;
